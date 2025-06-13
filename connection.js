2257192a7714ab588bae2e2ecd91485e39acc33b
TiffanuH <chaeung929@gmail.com>
2025-06-13 10:16:27 -0700

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,9 @@
+//connects frontend and backend code by fetching and stuff
+
+var express = require('express');
+var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
+var db = require('../db');
+
+var ensureLoggedIn = ensureLogIn();
+
+module.exports = router;
