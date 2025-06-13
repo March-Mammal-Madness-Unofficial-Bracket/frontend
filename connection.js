25dfdb846a48c0d802410d630caf0318665eeff1
TiffanuH <chaeung929@gmail.com>
2025-06-13 10:35:15 -0700

    Update connection.js
    starting to put more stuff; mostly incomplete but just for base structure

--- connection.js
+++ connection.js
@@ -6,7 +6,21 @@ var db = require('../db');
 
 var ensureLoggedIn = ensureLogIn();
 
+const express = require('express');
+const cors = require('cors'); // npm install cors
+const app = express();
 
+app.use(express.json()); 
+app.use(cors());
+
+//app.get <- fetch
+//app.post <- fetch
+
+const response = await fetch("backend.swiboe.org", { //additional port info unecessary
+  method: "POST",
+  body: JSON.stringify({ username: "example" }),
+  //(working)
+});
 
 
 module.exports = router;
