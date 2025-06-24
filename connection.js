4afaf38dfe714552f6320ae335dd67ab5cc2ffee
TiffanuH <chaeung929@gmail.com>
2025-06-24 11:24:39 -0700

    Update connection.js

--- connection.js
+++ connection.js
@@ -16,12 +16,23 @@ app.use(cors());
 //app.get <- fetch
 //app.post <- fetch
 
+
+//fetch format from GET request; researched briefly but not sure if this is actually correct
+fetch ('/api/brackets', {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+  }
+});
+
+
 const response = await fetch("backend.swiboe.org", { //additional port info unecessary
   method: "POST",
   headers: {
     "Content-Type": "application/json",
   },
   body: JSON.stringify({ username: "example" })
+
   //(working)
 });
 
